5e47d0842eca595ac9275a94ef7afa725b7ebc6d
Aishik Deb <debaishik14@gmail.com>
2020-06-08 21:51:00 +0530

    mongo uri add

---
+++ routes/user.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const sha256 = require('sha256');
+const nodemailer = require("nodemailer");
+var otpGenerator = require('otp-generator');
+let User = require('../models/user.model');
+
+router.route('/').get((req,res) => {
+    User.find()
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json('Error:' + err));
+});
+
+module.exports = router;
