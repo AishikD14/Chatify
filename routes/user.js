2e9d44e63f69f4acd5545debe2f5cc9c871c69e3
Suchana123 <me.suchana.mitra@gmail.com>
2020-06-19 11:16:08 +0530

    login api

--- routes/user.js
+++ routes/user.js
@@ -8,6 +8,22 @@ router.route('/').get((req,res) => {
     User.find()
         .then(users => res.json(users))
         .catch(err => res.status(400).json('Error:' + err));
+
+});
+router.route('/login').post((req,res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({email: email, password: password})
+        .then(users => {
+            if(!users){
+                res.status(203).json({"message":'Failure'});
+            }
+            else{
+                
+                    res.json({"message":'Success',})        
+            }
+        })
+        .catch(err => res.status(400).json('Error:' + err));
 });
 
 module.exports = router;
